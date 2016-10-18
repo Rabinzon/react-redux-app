752ad2d0faad4e26e511578240468a6f50e08f38
rabinzon <ildarrazin@gmail.com>
2016-10-18 20:11:58 +0300

    chore: setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/root/root';
+
+ReactDOM.render(
+  <App />, document.getElementById('app') // eslint-disable-line no-undef
+);
